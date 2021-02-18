29134ccdea3584a49d65645663d403c32530809e
unknown <cayelsie@gmail.com>
2021-02-17 19:27:51 -0500

    Employees.sql is schemata for the tables

---
+++ employees.sql
@@ -0,0 +1,54 @@
+
+--Create a new table for titles
+CREATE TABLE titles (
+	title_id VARCHAR(5) NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	PRIMARY KEY (title_id)
+	);
+
+-- Create a new table for employees
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date VARCHAR(10) NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(10) NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+	);
+	
+--Create a new table for departments
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (dept_no)
+	);
+
+--Create a new table for employees listed by department
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+	);
+
+--Create a new table for department managers
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+	);
+	
+--Create a new table for salaries
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+	);
+
